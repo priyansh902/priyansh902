52c2b3052638c996854cb44acad898f86f58783f
priyansh902 <pk953666@gmail.com>
2025-06-03 14:30:38 +0530

    new

---
+++ add.java
@@ -0,0 +1,15 @@
+package priyansh902;
+
+public class add {
+    public static void main(String[] args) {
+        int a = 5;
+       int b =7;
+        int c = a+b;
+        System.out.println(c);
+        System.out.println("adding method");
+        
+      
+    }
+    
+    
+}
---
+++ multiply.java
@@ -0,0 +1,8 @@
+package priyansh902;
+
+public class multiply {
+    public static void main(String[] args) {
+        System.out.println("multiply is");
+        
+    }
+}
