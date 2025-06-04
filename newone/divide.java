5b3a168dff3c50ffec34e45d84de79a2aa967bca
priyansh902 <pk953666@gmail.com>
2025-06-04 21:11:36 +0530

    new folder

---
+++ newone/divide.java
@@ -0,0 +1,11 @@
+package priyansh902.newone;
+
+public class divide {
+
+    public static void main(String[] args) {
+       int a = 6;
+       int b = 2;
+       int c = b/a;
+        System.out.println("quitent "  + c);
+    }
+}
